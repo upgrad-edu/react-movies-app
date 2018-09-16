c19998a77ef592eb2853a69b5ee4d8e3048ddd6c
Srishti Gupta <mail@srishtigupta.in>
2018-09-16 13:38:54 +0530

    Added 'Home' component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import Home from './screens/home/Home';
 
-ReactDOM.render(<div></div>, document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById('root'));
 registerServiceWorker();
