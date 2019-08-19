4ae4fca29f00f840ce4924781aa699e9e6eb2781
upgrad-edu <versioncontrol@upgrad.com>
2019-08-19 18:37:07 +0530

    Update Controller.js

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -9,7 +9,7 @@ class Controller extends Component {
 
   constructor() {
     super();
-    this.baseUrl = "http://18.212.74.66:8085/api/v1/";
+    this.baseUrl = "http://34.238.240.39:8080/api/v1/";
   }
   render() {
     return (
